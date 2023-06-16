82fa4a3093d14d6013e2f0acbc9c7fa98be3aa57
OTende <mezencev_e@inbox.ru>
2023-06-17 03:57:53 +0700

    add swipe adapter

---
+++ app/src/main/java/com/example/musicplayer/adapters/SwipeSongAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.musicplayer.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.musicplayer.data.entities.Song
+import com.example.musicplayer.databinding.SwipeItemBinding
+
+class SwipeSongAdapter : BaseSongAdapter<SwipeItemBinding>() {
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        attachToParent: Boolean
+    ): SwipeItemBinding {
+        return SwipeItemBinding.inflate(inflater, parent, attachToParent)
+    }
+
+    override fun bind(holder: SongViewHolder, song: Song) {
+        with(holder.binding) {
+            val text = "${song.title} - ${song.author}"
+            tvPrimary.text = text
+
+            root.setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+}
